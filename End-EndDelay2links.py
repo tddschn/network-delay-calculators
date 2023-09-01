749e21cc64d0afff3fbe4fc00764a9bf3c917ac1
Teddy Xinyuan Chen <45612704+tddschn@users.noreply.github.com>
2023-09-01 10:01:49 -0400

    use decimal

--- End-EndDelay2links.py
+++ End-EndDelay2links.py
@@ -1,57 +1,70 @@
 #!/usr/bin/env python3
+
 import argparse
+from decimal import Decimal, getcontext
 from tabulate import tabulate
 
-# Transmission Delay
+# Set precision
+getcontext().prec = 28
+
 def trans_delay(t, M):
-    return (M / t) * 10**3
+    """Compute Transmission Delay."""
+    return (M / t) * Decimal("1000")
 
-# Propagation Delay
 def prop_delay(d, S):
-    return d / (S * 100)
+    """Compute Propagation Delay."""
+    return d / (S * Decimal("100"))
 
-def main():
-    parser = argparse.ArgumentParser(description="Calculate network transmission and propagation delays.",
+def get_args():
+    """Get command line arguments."""
+    parser = argparse.ArgumentParser(description="Calculate network transmission and propagation delays for A-R-B network.", 
                                      formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument('--t1', type=float, default=1, help="Transmission Rate (link speed) at Link1 (value in Mbps)")
-    parser.add_argument('--t2', type=float, default=1, help="Transmission Rate (link speed) at Link2 (value in Mbps)")
-    parser.add_argument('--T1', type=float, help="Transmission Delay at Link1 (value in milliseconds)")
-    parser.add_argument('--T2', type=float, help="Transmission Delay at Link2 (value in milliseconds)")
-    parser.add_argument('--d1', type=float, default=1, help="Distance of Link1 (value in KM)")
-    parser.add_argument('--d2', type=float, default=1, help="Distance of Link2 (value in KM)")
-    parser.add_argument('--D1', type=float, help="Propagation Delay at Link1 (value in milliseconds)")
-    parser.add_argument('--D2', type=float, help="Propagation Delay at Link2 (value in milliseconds)")
-    parser.add_argument('-N', type=float, default=1, help="Number of Packets")
-    parser.add_argument('-M', type=float, default=1, help="Packet Size (value in Mbits)")
-    parser.add_argument('-S', type=float, default=1, help="Propagation Speed (speed in 10^8m/s)")
-    parser.add_argument('-p', type=float, default=0, help="Router Processing Time (processing time in milliseconds)")
+    parser.add_argument('--t1', type=Decimal, default=1, help="Transmission Delay (link speed) at Link1 (value in Mbps)")
+    parser.add_argument('--t2', type=Decimal, default=1, help="Transmission Delay (link speed) at Link2 (value in Mbps)")
 
-    args = parser.parse_args()
+    parser.add_argument('--T1', type=Decimal, help="Transmission Delay at Link1 (value in milliseconds)")
+    parser.add_argument('--T2', type=Decimal, help="Transmission Delay at Link2 (value in milliseconds)")
+
+    parser.add_argument('--d1', type=Decimal, default=1, help="Distance of Link1 (value in KM)")
+    parser.add_argument('--d2', type=Decimal, default=1, help="Distance of Link2 (value in KM)")
+    parser.add_argument('--D1', type=Decimal, help="Propagation Delay at Link1 (value in milliseconds)")
+    parser.add_argument('--D2', type=Decimal, help="Propagation Delay at Link2 (value in milliseconds)")
+
+    parser.add_argument('-N', type=Decimal, default=1, help="Number of Packets")
+    parser.add_argument('-M', type=Decimal, default=1, help="Packet Size (value in Mbits)")
+    parser.add_argument('-S', type=Decimal, default=1, help="Propagation Speed (speed in 10^8m/s)")
+    parser.add_argument('-p', type=Decimal, default=0, help="Router Processing Time (processing time in milliseconds)")
+
+    return parser.parse_args()
+
+def get_delays(args):
+    """Compute delays and return them."""
     data = []
+    n, A, queue_delay1 = Decimal("1"), Decimal("0"), Decimal("0")
 
-    n, A, B, queue_delay = 1, 0, 0, 0
     while n <= args.N:
-        R = A + (args.T1 if args.T1 else trans_delay(args.t1, args.M)) + (args.D1 if args.D1 else prop_delay(args.d1, args.S))
-        B = R + queue_delay + args.p + (args.T2 if args.T2 else trans_delay(args.t2, args.M)) + (args.D2 if args.D2 else prop_delay(args.d2, args.S))
-        
         td_t1 = args.T1 if args.T1 else trans_delay(args.t1, args.M)
         td_t2 = args.T2 if args.T2 else trans_delay(args.t2, args.M)
 
-        if R < td_t2:
-            queue_delay += td_t2 - td_t1
+        R = A + td_t1 + (args.D1 if args.D1 else prop_delay(args.d1, args.S))
+        B = R + queue_delay1 + args.p + td_t2 + (args.D2 if args.D2 else prop_delay(args.d2, args.S))
         
-        data.append(("P" + str(n), A, R, B))
+        if R < td_t2:
+            queue_delay1 += td_t2 - td_t1
 
-        # print(f'{"P" + str(n):<10}{"":<2}{A:9.3f} ms{"":<17}{R:9.3f} ms{"":<17}{B:9.3f} ms')
+        data.append(["P" + str(n), A, R, B, queue_delay1])
+
         n, A = n + 1, A + td_t1
 
-    print(f"\nEnd to End transmission delay = {B:9.3f} ms\n")
-    print()
-    headers = ["Packet", "A (ms)", "R (ms)", "B (ms)"]
-    # cSpell:disable
-    print(tabulate(data, headers=headers, floatfmt=".3f"))
-    # cSpell:enable
+    return data
 
+def main():
+    """Main function."""
+    args = get_args()
+    delays = get_delays(args)
+
+    headers = ["Packet", "A (ms)", "R (ms)", "B (ms)", "Queue Delay(ms)"]
+    print(tabulate(delays, headers=headers, tablefmt='pipe'))
 
 if __name__ == '__main__':
     main()
--- End-EndDelay3links.py
+++ End-EndDelay3links.py
@@ -1,72 +1,79 @@
 #!/usr/bin/env python3
 
 import argparse
+from decimal import Decimal, getcontext
 from tabulate import tabulate
 
+# Set precision
+getcontext().prec = 28
 
-
-# Transmission Delay
 def trans_delay(t, M):
-    return (float(M) / t) * 10**3
+    """Compute Transmission Delay."""
+    return (M / t) * Decimal("1000")
 
-# Propagation Delay
 def prop_delay(d, S):
-    return d / (S * 100.0)
+    """Compute Propagation Delay."""
+    return d / (S * Decimal("100"))
 
-def main():
+def get_args():
+    """Get command line arguments."""
     parser = argparse.ArgumentParser(description="Calculate network transmission and propagation delays.", 
                                      formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument('--t1', type=float, default=1, help="Transmission Delay (link speed) at Link1 (value in Mbps)")
-    parser.add_argument('--t2', type=float, default=1, help="Transmission Delay (link speed) at Link2 (value in Mbps)")
-    parser.add_argument('--t3', type=float, default=1, help="Transmission Delay (link speed) at Link3 (value in Mbps)")
-    
-    parser.add_argument('--T1', type=float, help="Transmission Delay at Link1 (value in milliseconds)")
-    parser.add_argument('--T2', type=float, help="Transmission Delay at Link2 (value in milliseconds)")
-    parser.add_argument('--T3', type=float, help="Transmission Delay at Link3 (value in milliseconds)")
+    parser.add_argument('--t1', type=Decimal, default=1, help="Transmission Delay (link speed) at Link1 (value in Mbps)")
+    parser.add_argument('--t2', type=Decimal, default=1, help="Transmission Delay (link speed) at Link2 (value in Mbps)")
+    parser.add_argument('--t3', type=Decimal, default=1, help="Transmission Delay (link speed) at Link3 (value in Mbps)")
 
-    parser.add_argument('--d1', type=float, default=1, help="Distance of Link1 (value in KM)")
-    parser.add_argument('--d2', type=float, default=1, help="Distance of Link2 (value in KM)")
-    parser.add_argument('--d3', type=float, default=1, help="Distance of Link3 (value in KM)")
-    parser.add_argument('--D1', type=float, help="Propagation Delay at Link1 (value in milliseconds)")
-    parser.add_argument('--D2', type=float, help="Propagation Delay at Link2 (value in milliseconds)")
-    parser.add_argument('--D3', type=float, help="Propagation Delay at Link3 (value in milliseconds)")
+    parser.add_argument('--T1', type=Decimal, help="Transmission Delay at Link1 (value in milliseconds)")
+    parser.add_argument('--T2', type=Decimal, help="Transmission Delay at Link2 (value in milliseconds)")
+    parser.add_argument('--T3', type=Decimal, help="Transmission Delay at Link3 (value in milliseconds)")
 
-    parser.add_argument('-N', type=float, default=1, help="Number of Packets")
-    parser.add_argument('-M', type=float, default=1, help="Packet Size (value in Mbits)")
-    parser.add_argument('-S', type=float, default=1, help="Propagation Speed (speed in 10^8m/s)")
-    parser.add_argument('-p', type=float, default=0, help="Router Processing Time (processing time in milliseconds)")
+    parser.add_argument('--d1', type=Decimal, default=1, help="Distance of Link1 (value in KM)")
+    parser.add_argument('--d2', type=Decimal, default=1, help="Distance of Link2 (value in KM)")
+    parser.add_argument('--d3', type=Decimal, default=1, help="Distance of Link3 (value in KM)")
+    parser.add_argument('--D1', type=Decimal, help="Propagation Delay at Link1 (value in milliseconds)")
+    parser.add_argument('--D2', type=Decimal, help="Propagation Delay at Link2 (value in milliseconds)")
+    parser.add_argument('--D3', type=Decimal, help="Propagation Delay at Link3 (value in milliseconds)")
 
-    args = parser.parse_args()
+    parser.add_argument('-N', type=Decimal, default=1, help="Number of Packets")
+    parser.add_argument('-M', type=Decimal, default=1, help="Packet Size (value in Mbits)")
+    parser.add_argument('-S', type=Decimal, default=1, help="Propagation Speed (speed in 10^8m/s)")
+    parser.add_argument('-p', type=Decimal, default=0, help="Router Processing Time (processing time in milliseconds)")
+
+
+    return parser.parse_args()
+
+def get_delays(args):
+    """Compute delays and return them."""
     data = []
+    n, A, queue_delay1, queue_delay2 = Decimal("1"), Decimal("0"), Decimal("0"), Decimal("0")
 
-    n, A, queue_delay1, queue_delay2 = 1, 0, 0, 0
     while n <= args.N:
-        R1 = A + (args.T1 if args.T1 else trans_delay(args.t1, args.M)) + (args.D1 if args.D1 else prop_delay(args.d1, args.S))
-        R2 = R1 + queue_delay1 + args.p + (args.T2 if args.T2 else trans_delay(args.t2, args.M)) + (args.D2 if args.D2 else prop_delay(args.d2, args.S))
-        B = R2 + queue_delay2 + args.p + (args.T3 if args.T3 else trans_delay(args.t3, args.M)) + (args.D3 if args.D3 else prop_delay(args.d3, args.S))
-        
-        
         td_t1 = args.T1 if args.T1 else trans_delay(args.t1, args.M)
         td_t2 = args.T2 if args.T2 else trans_delay(args.t2, args.M)
         td_t3 = args.T3 if args.T3 else trans_delay(args.t3, args.M)
 
+        R1 = A + td_t1 + (args.D1 if args.D1 else prop_delay(args.d1, args.S))
+        R2 = R1 + queue_delay1 + args.p + td_t2 + (args.D2 if args.D2 else prop_delay(args.d2, args.S))
+        B = R2 + queue_delay2 + args.p + td_t3 + (args.D3 if args.D3 else prop_delay(args.d3, args.S))
+        
         if R1 < td_t2:
             queue_delay1 += td_t2 - td_t1
         if R2 < td_t3:
             queue_delay2 += td_t3 - td_t2
 
-        data.append(("P" + str(n), A, R1, R2, B))
+        data.append(["P" + str(n), A, R1, R2, B, queue_delay1, queue_delay2])
 
-        # print(f'{"P" + str(n):<10}{"":<5}{A:9.3f}{"":<20}{R1:9.3f}{"":<20}{R2:9.3f}{"":<20}{B:9.3f}')
         n, A = n + 1, A + td_t1
 
-    # print()
-    # print("-----------------------------------------------------------------------------------------------------------------")
-    headers = ["Packet", "A (ms)", "R1 (ms)", "R2 (ms)", "B (ms)"]
-    # cSpell:disable
-    print(tabulate(data, headers=headers, floatfmt=".3f"))
-    # cSpell:enable
+    return data
 
+def main():
+    """Main function."""
+    args = get_args()
+    delays = get_delays(args)
+
+    headers = ["Packet", "A (ms)", "R1 (ms)", "R2 (ms)", "B (ms)", "Queue Delay1 (ms)", "Queue Delay2 (ms)"]
+    print(tabulate(delays, headers=headers, tablefmt='pipe'))
 
 if __name__ == '__main__':
     main()
